e488bc853d92f35bc124536362cb520d69dba100
dingsw-soy <26631931@qq.com>
2020-02-21 08:31:55 +0800

    sds repeat6剩余代码完成

--- repeat6/sds.c
+++ repeat6/sds.c
@@ -98,6 +98,122 @@ sds sdscat(sds s,const char *init){
     return sdscatlen(s,init,strlen(init));
 }
 
+sds sdscpylen(sds s,const char *t,size_t len){
+
+    struct sdshdr *sh = (void*)(s-(sizeof(struct sdshdr)));
+    size_t totlen;
+
+    totlen = sh->len+sh->free;
+    // 不满足长度,需扩容
+    if (totlen < len) {
+        s = sdsMakeRoomFor(s,len-totlen);
+        if (s == NULL) return NULL;
+        sh = (void*)(s-(sizeof(struct sdshdr)));
+        totlen = sh->len+sh->free;
+    }
+
+    // 拷贝字符串
+    memcpy(s,t,len);
+
+    // 添加终结符
+    s[len] = '\0';
+
+    // 更新属性
+    sh->free = totlen-len;
+    sh->len = len;
+
+    return s;
+}
+
+sds sdscpy(sds s,const char *t){
+    return sdscpylen(s,t,strlen(t));
+}
+
+sds sdsdup(sds s){
+    return sdsnewlen(s,strlen(s));
+}
+
+void sdstrim(sds s,const char *cset){
+
+    char *start,*end,*sp,*ep;
+    size_t len;
+    struct sdshdr *sh = (void*)(s-(sizeof(struct sdshdr)));
+    // 初始化起始和停止节点
+    start = sp = s;
+    end = ep = s+sh->len;
+
+    // 两端寻找指定字符
+    while(sp <= end && strchr(cset,*sp)) sp++;
+    while(ep >= start && strchr(cset,*ep)) ep--;
+
+    len = (sp > ep) ? 0 : (ep-sp)+1;
+    // 起始字符变更,移动字符串
+    if (start != sp) memmove(s,sp,len);
+
+    // 添加终结符
+    s[len] = '\0';
+
+    // 更新属性
+    sh->free += sh->len - len;
+    sh->len = len;
+}
+
+void sdsrange(sds s,int start,int end){
+
+    struct sdshdr *sh = (void*)(s-(sizeof(struct sdshdr)));
+    size_t len = sdslen(s),newlen;
+
+    if (len == 0) return;
+    // 负索引转正索引
+    if (start < 0){
+        start = len+start;
+        if (start < 0) start = 0;
+    }
+    if (end < 0){
+        end = len+end;
+        if (end < 0) end = 0;
+    }
+    // 计算截取后长度
+    newlen = (start > end) ? 0 : (end-start)+1;
+
+    // 最大值检查
+    if (newlen != 0) {
+        if (start > len) {
+            newlen = 0;
+        } else if(end > len){
+            end = len-1;
+            newlen = (start > end) ? 0 : (end-start)+1;
+        }
+    } else {
+        start = 0;
+    }
+
+    // 起始字符变更,移动字符串
+    if (start && newlen) memmove(s,s+start,newlen);
+    // 添加终结符
+    s[newlen] = '\0';
+    // 更新属性
+    sh->free += len - newlen;
+    sh->len = newlen;
+}
+
+int sdscmp(sds s1,sds s2){
+
+    size_t l1,l2,minLen;
+    int cmp;
+
+    // 获取字符串长度
+    l1 = sdslen(s1);
+    l2 = sdslen(s2);
+    minLen = (l1<l2) ? l1 : l2;
+    // 字符串最小长度比对
+    cmp = memcmp(s1,s2,minLen);
+    // 完整比对
+    if (cmp == 0) return l1-l2;
+
+    return cmp;
+}
+
 //执行: gcc -g zmalloc.c testhelp.h sds.c
 //执行: ./a.exe
 int main(void){
@@ -116,79 +232,79 @@ int main(void){
     test_cond("Strings concatenation",
         sdslen(x) == 5 && memcmp(x,"fobar\0",6) == 0);
 
-    // x = sdscpy(x,"a");
-    // test_cond("sdscpy() against an originally longer string",
-    //     sdslen(x) == 1 && memcmp(x,"a\0",2) == 0)
+    x = sdscpy(x,"a");
+    test_cond("sdscpy() against an originally longer string",
+        sdslen(x) == 1 && memcmp(x,"a\0",2) == 0)
 
-    // x = sdscpy(x,"xyzxxxxxxxxxxyyyyyyyyyykkkkkkkkkk");
-    // test_cond("sdscpy() against an originally shorter string",
-    //     sdslen(x) == 33 &&
-    //     memcmp(x,"xyzxxxxxxxxxxyyyyyyyyyykkkkkkkkkk\0",33) == 0)
+    x = sdscpy(x,"xyzxxxxxxxxxxyyyyyyyyyykkkkkkkkkk");
+    test_cond("sdscpy() against an originally shorter string",
+        sdslen(x) == 33 &&
+        memcmp(x,"xyzxxxxxxxxxxyyyyyyyyyykkkkkkkkkk\0",33) == 0)
     
-    // sdsfree(x);
-    // x = sdsnew("xxciaoyyy");
-    // sdstrim(x,"xy");
-    // test_cond("sdstrim() correctly trims characters",
-    //     sdslen(x) == 4 && memcmp(x,"ciao\0",5) == 0)
+    sdsfree(x);
+    x = sdsnew("xxciaoyyy");
+    sdstrim(x,"xy");
+    test_cond("sdstrim() correctly trims characters",
+        sdslen(x) == 4 && memcmp(x,"ciao\0",5) == 0)
 
-    // y = sdsdup(x);
-    // sdsrange(y,1,1);
-    // test_cond("sdsrange(...,1,1)",
-    //     sdslen(y) == 1 && memcmp(y,"i\0",2) == 0)
+    y = sdsdup(x);
+    sdsrange(y,1,1);
+    test_cond("sdsrange(...,1,1)",
+        sdslen(y) == 1 && memcmp(y,"i\0",2) == 0)
 
-    // sdsfree(y);
-    // y = sdsdup(x);
-    // sdsrange(y,1,-1);
-    // test_cond("sdsrange(...,1,-1)",
-    //     sdslen(y) == 3 && memcmp(y,"iao\0",4) == 0)
+    sdsfree(y);
+    y = sdsdup(x);
+    sdsrange(y,1,-1);
+    test_cond("sdsrange(...,1,-1)",
+        sdslen(y) == 3 && memcmp(y,"iao\0",4) == 0)
 
-    // sdsfree(y);
-    // y = sdsdup(x);
-    // sdsrange(y,-2,-1);
-    // test_cond("sdsrange(...,-2,-1)",
-    //     sdslen(y) == 2 && memcmp(y,"ao\0",3) == 0)
+    sdsfree(y);
+    y = sdsdup(x);
+    sdsrange(y,-2,-1);
+    test_cond("sdsrange(...,-2,-1)",
+        sdslen(y) == 2 && memcmp(y,"ao\0",3) == 0)
 
-    // sdsfree(y);
-    // y = sdsdup(x);
-    // sdsrange(y,2,1);
-    // test_cond("sdsrange(...,2,1)",
-    //     sdslen(y) == 0 && memcmp(y,"\0",1) == 0)
+    sdsfree(y);
+    y = sdsdup(x);
+    sdsrange(y,2,1);
+    test_cond("sdsrange(...,2,1)",
+        sdslen(y) == 0 && memcmp(y,"\0",1) == 0)
 
-    // sdsfree(y);
-    // y = sdsdup(x);
-    // sdsrange(y,1,100);
-    // test_cond("sdsrange(...,1,100)",
-    //     sdslen(y) == 3 && memcmp(y,"iao\0",4) == 0)
+    sdsfree(y);
+    y = sdsdup(x);
+    sdsrange(y,1,100);
+    test_cond("sdsrange(...,1,100)",
+        sdslen(y) == 3 && memcmp(y,"iao\0",4) == 0)
 
-    // sdsfree(y);
-    // y = sdsdup(x);
-    // sdsrange(y,100,100);
-    // test_cond("sdsrange(...,100,100)",
-    //     sdslen(y) == 0 && memcmp(y,"\0",1) == 0)
+    sdsfree(y);
+    y = sdsdup(x);
+    sdsrange(y,100,100);
+    test_cond("sdsrange(...,100,100)",
+        sdslen(y) == 0 && memcmp(y,"\0",1) == 0)
 
-    // sdsfree(y);
-    // sdsfree(x);
-    // x = sdsnew("foo");
-    // y = sdsnew("foa");
-    // test_cond("sdscmp(foo,foa)", sdscmp(x,y) > 0)
+    sdsfree(y);
+    sdsfree(x);
+    x = sdsnew("foo");
+    y = sdsnew("foa");
+    test_cond("sdscmp(foo,foa)", sdscmp(x,y) > 0)
 
-    // sdsfree(y);
-    // sdsfree(x);
-    // x = sdsnew("bar");
-    // y = sdsnew("bar");
-    // test_cond("sdscmp(bar,bar)", sdscmp(x,y) == 0)
+    sdsfree(y);
+    sdsfree(x);
+    x = sdsnew("bar");
+    y = sdsnew("bar");
+    test_cond("sdscmp(bar,bar)", sdscmp(x,y) == 0)
 
-    // sdsfree(y);
-    // sdsfree(x);
-    // x = sdsnew("aar");
-    // y = sdsnew("bar");
-    // test_cond("sdscmp(aar,bar)", sdscmp(x,y) < 0)
+    sdsfree(y);
+    sdsfree(x);
+    x = sdsnew("aar");
+    y = sdsnew("bar");
+    test_cond("sdscmp(aar,bar)", sdscmp(x,y) < 0)
 
-    // sdsfree(y);
-    // sdsfree(x);
-    // x = sdsnew("bara");
-    // y = sdsnew("bar");
-    // test_cond("sdscmp(bara,bar)", sdscmp(x,y) > 0)
+    sdsfree(y);
+    sdsfree(x);
+    x = sdsnew("bara");
+    y = sdsnew("bar");
+    test_cond("sdscmp(bara,bar)", sdscmp(x,y) > 0)
 
     return 0;
 }
