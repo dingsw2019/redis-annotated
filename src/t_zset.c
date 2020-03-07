dac8883b0383ad1c3f87bceeeb8917df6983428e
dingsw-soy <26631931@qq.com>
2020-03-07 10:41:30 +0800

    zslInsert

--- src/t_zset.c
+++ src/t_zset.c
@@ -105,3 +105,97 @@ int zslRandomLevel(void)
 
     return (level<ZSKIPLIST_MAXLEVEL) ? level : ZSKIPLIST_MAXLEVEL;
 }
+
+zskiplistNode *zslInsert(zskiplist *zsl, double score, robj *obj)
+{
+    zskiplistNode *update[ZSKIPLIST_MAXLEVEL], *x;
+    unsigned int rank[ZSKIPLIST_MAXLEVEL];
+    int i, level;
+
+    x = zsl->header;
+    // 遍历层
+    for(i=zsl->level-1; i>=0; i--){
+
+        // 设置已跳过的节点数
+        // 下面只循环 x ,所以之前楼层跳过的节点,不会再遍历
+        // 所以这里直接赋值上面跳过的节点,在此基础上,再查找是否存在跳过的节点
+        rank[i] = (i == (zsl->level-1)) ? 0 : rank[i+1];
+
+        // 遍历节点,查找可添加的位置
+        // 1.按分值查找,跳过小于 score 的节点
+        // 2.如果 score 相同,跳过成员值小的
+        while(x->level[i].forward && 
+                // 比对分值
+               (x->level[i].forward->score < score ||
+                // 比对成员
+                (x->level[i].forward->score == score && 
+                 compareStringObj(x->level[i].forward->obj,obj)<0
+             ))
+        )
+        {
+            // 更新跳过节点数
+            rank[i] += x->level[i].span;
+            // 处理下一个节点
+            x = x->level[i].forward;
+        }
+
+        // 记录可添加的节点位置之前的节点
+        update[i] = x;
+    }
+
+    // 生成随机层
+    level = zslRandomLevel();
+
+    // 初始化未设置的层
+    if (level > zsl->level) {
+        for(i=zsl->level; i<level; i++){
+            rank[i] = 0;
+            update[i] = zsl->header;
+            update[i]->level[i].span = zsl->length;
+        }
+
+        // 更新跳跃表最大层数
+        zsl->level = level;
+    }
+
+    // 创建新节点
+    x = zslCreateNode(level,score,obj);
+
+    // 设置层属性
+    for (i=0; i<level; i++){
+
+        // 更新"新节点的前进指针"指向
+        x->level[i].forward = update[i]->level[i].forward;
+
+        // 更新"新节点的前一个节点的前进指针"指向
+        update[i]->level[i].forward = x;
+
+        // 更新"新节点"的跨度
+        x->level[i].span = update[i]->level[i].span - (rank[0] - rank[i]);
+
+        // 更新"新节点的前一个节点"的跨度
+        // 其中 +1 是新节点的跨度
+        update[i]->level[i].span = (rank[0] - rank[i]) + 1;
+    }
+
+    // 更新"未接触的节点"的 span 值
+    // 比如:level 小于 zsl->level 可能会出现未接触节点
+    // 但是添加节点后,原先span就少了,所以要更新
+    for (i = level; i < zsl->level; i++) {
+        update[i]->level[i].span++;
+    }
+
+    // 设置"新节点"的后退指针
+    x->backward = (update[0] == zsl->header) ? NULL : update[0];
+    // 更新"新节点的下一个节点"的后退指针的指向
+    if (x->level[0].forward)
+        x->level[0].forward->backward = x;
+    // 如果"新节点"是最后一个节点,更新跳跃表的尾节点指针指向
+    else
+        zsl->tail = x;
+
+    // 更新节点数
+    zsl->length++;
+
+    return x;
+}
